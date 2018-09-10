df45472afcea2da6ebf5c8cb1fbda54198600f68
Kuniwak <orga.chem.job@gmail.com>
2018-09-10 13:02:46 +0900

    Initial Commit

---
+++ DIPlayground/DIPlayground.h
@@ -0,0 +1,19 @@
+//
+//  DIPlayground.h
+//  DIPlayground
+//
+//  Created by Yuki Kokubun on 2018/09/10.
+//  Copyright © 2018年 Yuki Kokubun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DIPlayground.
+FOUNDATION_EXPORT double DIPlaygroundVersionNumber;
+
+//! Project version string for DIPlayground.
+FOUNDATION_EXPORT const unsigned char DIPlaygroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DIPlayground/PublicHeader.h>
+
+
